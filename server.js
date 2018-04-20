935463d9ac248aa5d1f715e6d6b668d32c67a5ed
Tan Kai Wei <tankaiwei@Tans-MacBook-Pro.local>
2018-04-20 17:44:13 +0800

    change server.js to original

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
-// app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 8080);
 
-// var server = app.listen(app.get('port'), function() {
-//   console.log('listening on port ', server.address().port);
-// });
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
+});
 
-app.listen(process.env.PORT || 8080)

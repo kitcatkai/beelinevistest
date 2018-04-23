9940e915fb3f54be50b53f8f486451ca20c76066
Tan Kai Wei <tankaiwei@Tans-MacBook-Pro.local>
2018-04-23 14:20:02 +0800

    fix the port

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ var app = express();
 
 
 app.set('port', process.env.PORT || 8080);
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 
 // views is directory for all template files
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/app.js');
-});
+// app.get('/', function(request, response) {
+//   response.sendFile(__dirname + '/app.js');
+// });
 
 
 app.listen(app.get('port'), function() {
-  console.log('listening on port', app.get(;port) );
+  console.log('listening on port', app.get('port'));
 });
 
 // console.log('listening to port', process.env.PORT);
